b3523cf45269ac9cba991d7ac7007b732946da7f
Eric Neunaber <eneunaber@gmail.com>
2014-03-12 22:58:56 -0500

    Made url more generic and added more logging

--- standard-draft-pre-rank-players.user.js
+++ standard-draft-pre-rank-players.user.js
@@ -2,12 +2,12 @@
 // @name           	Pre-Rank Players For Yahoo Standard Draft
 // @namespace      	com.yahoo.fantasy.baseball
 // @copyright      	2012-13, Eric Neunaber, Mays Copeland (http://www.lastplayerpicked.com) and Bert Sanders
-// @include         http://baseball.fantasysports.yahoo.com/b1/127440/1/editprerank*
+// @include         http://baseball.fantasysports.yahoo.com/b1/*/*/editprerank*
 // @grant           GM_xmlhttpRequest
 // @require  	    	http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.1.min.js
 // ==/UserScript==
 (function() {
-   var priceGuideURL = http://www.draftbuddy.com/baseball/lastplayerpicked/index.php?t=14&l=MLB&m=260&b=1&ds=14E&dis=500&spl=&hs=70&ps=30&R=Y&RBI=Y&OBP=Y&BB=Y&TB=Y&ERA=Y&WHIP=Y&PHR=Y&K9=Y&KBB=Y&IP=Y&C=1&1B=1&2B=1&3B=1&SS=1&OF=0&LF=1&CF=1&RF=1&CI=0&MI=0&IF=0&Util=1&mg=10&SP=6&RP=3&P=0&ms=5&mr=5&o=S";
+   var priceGuideURL = "http://www.draftbuddy.com/baseball/lastplayerpicked/index.php?t=14&l=MLB&m=260&b=1&ds=14E&dis=500&spl=&hs=70&ps=30&R=Y&RBI=Y&OBP=Y&BB=Y&TB=Y&ERA=Y&WHIP=Y&PHR=Y&K9=Y&KBB=Y&IP=Y&C=1&1B=1&2B=1&3B=1&SS=1&OF=0&LF=1&CF=1&RF=1&CI=0&MI=0&IF=0&Util=1&mg=10&SP=6&RP=3&P=0&ms=5&mr=5&o=S";
    var players = new Array();
 
    getPlayers();
@@ -69,6 +69,7 @@
 
    function getPlayers()
    {
+	  console.log("GM_xmlhttpRequest...");
       GM_xmlhttpRequest(
       {
           method: 'GET',
@@ -78,6 +79,10 @@
             buildPlayersHash(responseDetails.responseText);
             populatePrices();
           },
+		  onerror: function( responseDetails )
+          {
+            console.log("bad things happened...");
+          }
       });
    }
 
